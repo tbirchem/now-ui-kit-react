ebda92d48875f67d7b40c2b9766ada646008a120
tbirchem <taylor.birhem@icloud.com>
2022-08-21 09:12:13 -0700

    Footer and Sign up

--- src/components/Footers/DarkFooter.js
+++ src/components/Footers/DarkFooter.js
@@ -2,60 +2,28 @@
 import React from "react";
 
 // reactstrap components
-import { Container } from "reactstrap";
+import {Container} from "reactstrap";
 
-function DarkFooter() {
-  return (
-    <footer className="footer" data-background-color="black">
-      <Container>
-        <nav>
-          <ul>
-            <li>
-              <a
-                href="https://www.creative-tim.com?ref=nukr-dark-footer"
+function Footer() {
+  return (<footer className="footer" data-background-color="black">
+    <Container>
+      <nav>
+        <ul>
+          <li>
+            <a
+                href="https://www.google.com/maps/place/Suncrest+Landscaping+%26+Design,+Inc./@46.336059,-122.5774749,9.18z/data=!4m5!3m4!1s0x0:0x7d779890040463c5!8m2!3d46.1134669!4d-122.367359"
                 target="_blank"
-              >
-                Creative Tim
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://presentation.creative-tim.com?ref=nukr-dark-footer"
-                target="_blank"
-              >
-                About Us
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://blog.creative-tim.com?ref=nukr-dark-footer"
-                target="_blank"
-              >
-                Blog
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="copyright" id="copyright">
-          © {new Date().getFullYear()}, Designed by{" "}
-          <a
-            href="https://www.invisionapp.com?ref=nukr-dark-footer"
-            target="_blank"
-          >
-            Invision
-          </a>
-          . Coded by{" "}
-          <a
-            href="https://www.creative-tim.com?ref=nukr-dark-footer"
-            target="_blank"
-          >
-            Creative Tim
-          </a>
-          .
-        </div>
-      </Container>
-    </footer>
-  );
+            >
+              Suncrest Landscaping & Design inc.
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="copyright" id="copyright">
+        CONTACT US: TAYLOR.BIRCHEM@ICLOUD.COM
+      </div>
+    </Container>
+  </footer>);
 }
 
-export default DarkFooter;
+export default Footer;
