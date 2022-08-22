4dc76f6016cb556cee7d3bfcf37d98a16b6f2453
tbirchem <taylor.birhem@icloud.com>
2022-08-21 22:38:37 -0700

    Deleting landingPage.js loginPage.js profilePage.js

--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ import "assets/demo/nucleo-icons-page-styles.css?v=1.5.0";
 // pages for this kit
 import Index from "views/Index.js";
 import NucleoIcons from "views/NucleoIcons.js";
-import LoginPage from "views/examples/LoginPage.js";
 import LandingPage from "views/examples/LandingPage.js";
 import ProfilePage from "views/examples/ProfilePage.js";
 
@@ -38,22 +37,6 @@ root.render(
     <Switch>
       <Switch>
         <Route path="/index" render={(props) => <Index {...props} />} />
-        <Route
-          path="/nucleo-icons"
-          render={(props) => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page"
-          render={(props) => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={(props) => <ProfilePage {...props} />}
-        />
-        <Route
-          path="/login-page"
-          render={(props) => <LoginPage {...props} />}
-        />
         <Redirect to="/index" />
         <Redirect from="/" to="/index" />
       </Switch>
