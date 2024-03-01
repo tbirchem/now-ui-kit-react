92c3c1e508afc311439be929b528a6535ea200b4
tbirchem <taylor.birhem@icloud.com>
2024-03-01 09:47:45 -0800

    routing issues

--- src/index.js
+++ src/index.js
@@ -33,8 +33,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BrowserRouter>
     <Switch>
             <Route path="/index" render={(props) => <Index {...props} />} />
-            <Route path="/about" element={<About/>}/>
-            {/*<Route path="/about" render={(props) => <About {...props} />} />*/}
+            <Route basename="/about" render={(props) => <About {...props} />} />
             <Route path="/" render={(props) => <Index {...props} />}/>
             <Route path="" render={(props) => <Index {...props} />}/>
     </Switch>
